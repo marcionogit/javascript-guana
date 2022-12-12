7a5ab91931aa7ae9262d41c3360b7c299963046f
Marcio Cesar <marcio0604982gmail.com>
2022-12-11 22:42:27 -0300

    novas imagens / organizando o codigo

--- exercicios/ex001/script.js
+++ exercicios/ex001/script.js
@@ -1,28 +1,50 @@
+// PEGANDO AS DATAS
 const dataNasc = document.querySelector('input#ano');
 const idade = 0
+
+// BOTÔES DE GÊNEROS
 const masculino = document.querySelector('input#masculino');
 const feminino = document.querySelector('input#feminino');
+
+// BOTÃO DE ATIVAR A FUNÇÃO
 const verificar = document.querySelector('input#verificar');
+
+// TEXTO QUE SERÁ ESCRITO A IDADE
 const resultado = document.querySelector('div#resultado');
-const imgHomem = document.querySelector('#imghomem')
-const imgMulher = document.querySelector('#imgmulher')
 
 
-let faixaEtaria = '';
+
+// IMAGENS HOMENS
+const imgHomemMenor = document.querySelector('#imghomem-menor');
+const imgHomemAdulto = document.querySelector('#imghomem-adulto');
+const imgHomemVelho = document.querySelector('#imghomem-velho');
+
+
+// IMAGENS MULHERES
+const imgMulherAdulta = document.querySelector('#imgmulher-adulta');
+
+
+// EVENTO DE CLIQUE NO BOTÃO
 verificar.addEventListener('click', function(){
     
-    
+// CALCULO IDADE
     const idade = 2022 - Number(dataNasc.value);
-    if(idade < 18){
-        faixaEtaria = 'não é adulto';
-    } else if(idade >= 18){
-        faixaEtaria = 'adulto(a)'
-    }; 
 
-    if(idade != 2022 && masculino.checked){
-    resultado.innerHTML = `Você tem ${idade} anos e é um homem ${faixaEtaria}`;
-    imgHomem.classList.toggle('ativado');
-    } else if(idade != 2022 && feminino.checked){
+// QUAL IMAGEM MOSTRAR SE FOR HOMEM    
+    if(idade != 2022 && idade >= 65 && masculino.checked){
+        imgHomemVelho.classList.toggle('ativado');
+        resultado.innerHTML = `Você é um senhor de ${idade} anos de idade`;
+    }else if(idade != 2022 && idade > 18 && masculino.checked){
+        imgHomemAdulto.classList.toggle('ativado');
+        resultado.innerHTML = `Você é um homem de ${idade} anos de idade`;
+    }else if(idade != 2022 && idade <= 18 && masculino.checked){
+        imgHomemMenor.classList.toggle('ativado');
+        resultado.innerHTML = `Você é um jovem de ${idade} anos de idade`;
+    }
+    
+    
+// QUAL IMAGEM MOSTRAR SE FOR MULHER
+    else if(idade != 2022 && feminino.checked){
     resultado.innerHTML = `Você tem ${idade} anos e é uma mulher ${faixaEtaria}`;
     imgMulher.classList.toggle('ativado');
     }
